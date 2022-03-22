7a172c71703489cc850cce124cf7d6afea0ba46a
fly206 <flyflyan@outlook.com>
2022-03-23 01:33:30 +0800

    update blogs

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220323.0119';
+const cacheName = 'chirpy-20220323.0128';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
