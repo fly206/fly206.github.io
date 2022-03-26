2b1c94eea58977065a66a34511450474f8488646
fly206 <flyflyan@outlook.com>
2022-03-26 14:38:59 +0800

    修复一处文本错误

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220324.2217';
+const cacheName = 'chirpy-20220326.1437';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
