407696be35d158fa1ae83e7594ea94007d4979f3
fly206 <flyflyan@outlook.com>
2022-04-02 21:24:16 +0800

    notes add

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220327.1423';
+const cacheName = 'chirpy-20220402.2122';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
